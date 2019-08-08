3527f7ed80f6c029b0500491a24b4009045d1027
lucas <lucasmoura765@gmail.com>
2019-08-08 00:07:32 -0300

    getting the score + title and printing if score is bigger than 5000

--- crawlers/app.py
+++ crawlers/app.py
@@ -1,10 +1,28 @@
 import requests
+from bs4 import BeautifulSoup
 
-subreddit = 'GlobalOffensive'
+subreddit = 'cats'
 url = 'https://old.reddit.com/r/'
 
-page = requests.get(url + subreddit, headers = {'User-agent': 'python-crawler:1.0.0'})
+thread_count = 0
 
-print(page.text)
+raw_page = requests.get(url + subreddit, headers = {'User-agent': 'python-crawler:1.0.0'})
 
-print('yeah this is working fine')
+soup = BeautifulSoup(raw_page.text, 'html.parser')
+
+# print(soup.prettify())
+threads = soup.find_all(class_='thing')
+
+for thread in threads:
+    thread_count += 1
+
+    score = int(thread.get('data-score'))
+    if(score >= 5000):
+        # print(thread.attrs)
+        title = thread.find('a', class_='title').get_text()
+        line = str(score) + ' - ' + title
+        print(line)
+    
+        
+last_id = thread.get('id').split('thing_t3_')[1]
+print('last id: ' + last_id)
